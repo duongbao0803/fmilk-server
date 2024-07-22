12006751b121f2f817ce8e493dfe49a8efe24252
Duong <tonbao0803@gmail.com>
2024-07-22 18:01:18 +0700

    fix: Fix config database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const User = require("../models/user");
 const Product = require("../models/product");
 const Post = require("../models/post");
 const Order = require("../models/order");
 const Brand = require("../models/brand");
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
